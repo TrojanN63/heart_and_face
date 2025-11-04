e90b2af44a8d6603104d48c3fe6445ffa2e264ce
TrojanN63 <arthuralvestav@gmail.com>
2025-11-04 19:41:06 -0300

    Corrigindo erro de reatribuição de valor e desenhando distância calculada.

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ video = cv.VideoCapture(0)
 start_time = time.perf_counter()
 mp_draw = mp.solutions.drawing_utils
 mp_face = mp.solutions.face_mesh
-spec_landmark = mp_draw.DrawingSpec(color=(0,255,0), thickness=1, circle_radius=1)
-spec_connection = mp_draw.DrawingSpec(color=(0,0,255), thickness=1)
+spec_landmark = mp_draw.DrawingSpec(color=(0,255,0), thickness=0, circle_radius=0)
+spec_connection = mp_draw.DrawingSpec(color=(0,0,255), thickness=2)
 content = 'ypos,time\n'
 
 with mp_face.FaceMesh(refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5) as face:
@@ -24,20 +24,21 @@ with mp_face.FaceMesh(refine_landmarks=True, min_detection_confidence=0.5, min_t
             for face_landmark in results_f.multi_face_landmarks:
                 mp_draw.draw_landmarks(frame,
                                        face_landmark,
-                                       mp_face.FACEMESH_CONTOURS,
+                                       mp_face.FACEMESH_RIGHT_EYE,
                                        landmark_drawing_spec=spec_landmark,
                                        connection_drawing_spec=spec_connection)
             height, width, _ = frame.shape
             landmarks = results_f.multi_face_landmarks[0]
-            lm_158 = landmarks.landmark[158]
+            lm_159 = landmarks.landmark[159]
             lm_468 = landmarks.landmark[468]
             # print(f"LandMark 158 position: {lm_158.x},{lm_158.y},{lm_158.z}")
-            (x158,y158,y158) = lm_158.x, lm_158.y, lm_158.z
+            (x159,y159,z159) = lm_159.x, lm_159.y, lm_159.z
             (x468,y468,z468) = lm_468.x, lm_468.y, lm_468.z
+            cv.line(frame, (int(x159*width),int(y159*height)), (int(x468*width),int(y468*height)), (255,255,255), 2)
             # print(y468)
             end_time = time.perf_counter()
             timer = end_time - start_time
-            content += f'{y468-y158},{timer}\n'
+            content += f'{y468-y159},{timer}\n'
             with open('data.csv','w') as file:
                 file.write(content)
         if istrue:
